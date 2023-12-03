5d80f00651c3f83435889e67d3a3ba898b0f43ca
it1221 <isit1221@gmail.com>
2023-12-03 18:20:32 +0900

    Add placeholder

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -25,12 +25,12 @@
                     <div class="form-group">
                         <label for="title">件名</label>
                         <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}"
-                            placeholder="Enter Title">
+                            placeholder="件名を入力してください">
                     </div>
 
                     <div class="form-group">
                         <label for="body">本文</label>
-                        <textarea name="body" class="form-control" id="body" cols="30" rows="10">{{ old('body') }}</textarea>
+                        <textarea name="body" class="form-control" id="body" cols="30" rows="10" placeholder="本文を入力してください">{{ old('body') }}</textarea>
                     </div>
 
                     <div class="form-group">
